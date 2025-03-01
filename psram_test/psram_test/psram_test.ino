3d5ea064f15ab3882f9a9eea89efd44bbf8c25c9
Nick Waddoups <nick.waddoups@usu.edu>
2025-03-01 11:48:29 -0700

    Added PSRAM testing sketch

---
+++ psram_test/psram_test/psram_test.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.printf("Total heap: %d\n", ESP.getHeapSize());
+  Serial.printf("Free heap: %d\n", ESP.getFreeHeap());
+  Serial.printf("Total PSRAM: %d\n", ESP.getPsramSize());
+  Serial.printf("Free PSRAM: %d\n", ESP.getFreePsram());
+}
+
+void loop() {}
